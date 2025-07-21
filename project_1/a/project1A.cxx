a34a65b8cbbcac52ee3f340d9468ec0c51710971
caarrp <junebirdsfly@gmail.com>
2025-07-21 13:15:26 -0700

    for next time files for the projects

---
+++ project_1/a/project1A.cxx
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vtkDataSet.h>
+#include <vtkImageData.h>
+#include <vtkPNGWriter.h>
+#include <vtkPointData.h>
+
+using std::cerr;
+using std::endl;
+
+vtkImageData *
+NewImage(int height, int width)
+{
+    vtkImageData *img = vtkImageData::New();
+    img->SetDimensions(width, height, 1);
+    img->AllocateScalars(VTK_UNSIGNED_CHAR, 3);
+
+    return img;
+}
+
+void
+WriteImage(vtkImageData *img, const char *filename)
+{
+   std::string full_filename = filename;
+   full_filename += ".png";
+   vtkPNGWriter *writer = vtkPNGWriter::New();
+   writer->SetInputData(img);
+   writer->SetFileName(full_filename.c_str());
+   writer->Write();
+   writer->Delete();
+}
+
+
+int main()
+{
+   std::cerr << "In main!" << endl;
+   vtkImageData *image = NewImage(1024, 1024);
+   unsigned char *buffer = 
+     (unsigned char *) image->GetScalarPointer(0,0,0);
+   buffer[0] = 255;
+   WriteImage(image, "proj1A");
+}
